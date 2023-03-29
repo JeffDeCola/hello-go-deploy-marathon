e98d86d0704a7b2291b8578e5096654a05bc7c0d
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-03-29 16:24:15 -0400

    updated ci

--- erase-ci/destroy-pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# hello-go-deploy-marathon destroy-pipeline.sh
-
-echo " "
-echo "Destroy pipeline on target jeffs-ci-target which is team jeffs-ci-team"
-fly --target jeffs-ci-target \
-    destroy-pipeline \
-    --pipeline hello-go-deploy-marathon
-echo " "
